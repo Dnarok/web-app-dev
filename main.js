bc47aa78efb780cb105f4c62ec05c4e7d0f49f5b
dnarok <jeppreston@gmail.com>
2020-11-09 03:16:49 -0500

    Wow

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-function func()
+var turn = 0;
+
+function clicked(i)
 {
-    window.alert("Trying to click random buttons, eh?");
-    console.log("I don't actually care.");
+    if (i.src.endsWith("images/empty.png"))
+    {
+        if (turn % 2 == 0)
+        {
+            i.src = "images/x.png";
+        }
+        else
+        {
+            i.src = "images/o.png";
+        }
+        turn++;
+    }
+}
+
+function reset()
+{
+    turn = 0;
+
+    document.getElementById("tic1").src = "images/empty.png";
+    document.getElementById("tic2").src = "images/empty.png";
+    document.getElementById("tic3").src = "images/empty.png";
+    document.getElementById("tac1").src = "images/empty.png";
+    document.getElementById("tac2").src = "images/empty.png";
+    document.getElementById("tac3").src = "images/empty.png";
+    document.getElementById("toe1").src = "images/empty.png";
+    document.getElementById("toe2").src = "images/empty.png";
+    document.getElementById("toe3").src = "images/empty.png";
 }
